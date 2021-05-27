937854b7c8684aaf0daf3514d899ed1815c54301
hyengchan <hyengchan@39degreesc.com>
2021-05-27 09:57:30 +0900

    main view model

--- RxWeather/ViewModel/MainViewModel.swift
+++ RxWeather/ViewModel/MainViewModel.swift
@@ -29,6 +29,17 @@ import NSObject_Rx
 typealias SectionModel = AnimatableSectionModel<Int, WeatherData>
 
 class MainViewModel: HasDisposeBag {
+    init(title: String, sceneCoordinator: SceneCoordinatorType, weatherApi: WeatherApiType, locationProvider: LocationProviderType) {
+        self.title = BehaviorRelay(value: title)
+        self.sceneCoordinator = sceneCoordinator
+        self.weatherApi = weatherApi
+        self.locationProvider = locationProvider
+        
+        locationProvider.currentAddress()
+            .bind(to: self.title)
+            .disposed(by: disposeBag)
+    }
+    
    static let tempFormatter: NumberFormatter = {
       let formatter = NumberFormatter()
       formatter.minimumFractionDigits = 0
@@ -43,5 +54,48 @@ class MainViewModel: HasDisposeBag {
       return formatter
    }()
    
+    let title: BehaviorRelay<String>
+    
+    let sceneCoordinator: SceneCoordinatorType
+    let weatherApi: WeatherApiType
+    let locationProvider: LocationProviderType
    
+    var weatherData: Driver<[SectionModel]> {
+        return locationProvider.currentLocation()
+            .flatMap { [unowned self] in
+                self.weatherApi.fetch(location: $0)
+                    .asDriver(onErrorJustReturn: (nil, [WeatherDataType]()))
+            }
+            .map { (summary, forecast) in
+                var summaryList = [WeatherData]()
+                if let summary = summary as? WeatherData {
+                    summaryList.append(summary)
+                }
+                
+                return [
+                    SectionModel(model: 0, items: summaryList),
+                    SectionModel(model: 1, items: forecast as! [WeatherData])
+                ]
+            }
+            .asDriver(onErrorJustReturn: [])
+    }
+    
+    let dataSource: RxTableViewSectionedAnimatedDataSource<SectionModel> = {
+        let ds = RxTableViewSectionedAnimatedDataSource<SectionModel> { (dataSource, tableView, indexPath, data) -> UITableViewCell in
+            
+            switch indexPath.section {
+            case 0:
+                let cell = tableView.dequeueReusableCell(withIdentifier: SummaryTableViewCell.identifier, for: indexPath) as! SummaryTableViewCell
+                cell.configure(from: data, tempFormatter: MainViewModel.tempFormatter)
+                return cell
+                
+            default:
+                let cell = tableView.dequeueReusableCell(withIdentifier: ForecastTableViewCell.identifier, for: indexPath) as! ForecastTableViewCell
+                cell.configure(from: data, dateFormatter: MainViewModel.dateFormatter, tempFormatter: MainViewModel.tempFormatter)
+                return cell
+            }
+            
+        }
+        return ds
+    } ()
 }
