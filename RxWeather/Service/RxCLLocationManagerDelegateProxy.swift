564c37256d45c74796189a7ece669a728a99e625
hyengchan <hyengchan@39degreesc.com>
2021-05-28 15:40:38 +0900

    delegate proxy

--- RxWeather/Service/RxCLLocationManagerDelegateProxy.swift
+++ RxWeather/Service/RxCLLocationManagerDelegateProxy.swift
@@ -24,3 +24,49 @@ import UIKit
 import CoreLocation
 import RxSwift
 import RxCocoa
+
+extension CLLocationManager: HasDelegate {
+    public typealias Delegate = CLLocationManagerDelegate
+}
+
+
+public class RxCLLocationManagerDelegateProxty: DelegateProxy<CLLocationManager, CLLocationManagerDelegate>, DelegateProxyType, CLLocationManagerDelegate {
+    
+    public init(locationManager: CLLocationManager) {
+        super.init(parentObject: locationManager, delegateProxy: RxCLLocationManagerDelegateProxty.self)
+    }
+    
+    public static func registerKnownImplementations() {
+        self.register { RxCLLocationManagerDelegateProxty(locationManager: $0) }
+    }
+}
+
+
+extension Reactive where Base: CLLocationManager {
+    var delegate: DelegateProxy<CLLocationManager, CLLocationManagerDelegate> {
+        return RxCLLocationManagerDelegateProxty.proxy(for: base)
+    }
+    
+    public var didUpdateLocation: Observable<[CLLocation]> {
+        let sel = #selector(CLLocationManagerDelegate.locationManager(_:didUpdateLocations:))
+        return delegate.methodInvoked(sel)
+            .map { parameters in
+                return parameters[1] as! [CLLocation]
+            }
+    }
+    
+    public var didChangeAuthorizationStatus: Observable<CLAuthorizationStatus> {
+        let sel: Selector
+        
+        if #available(iOS 14.0, *) {
+            sel = #selector(CLLocationManagerDelegate.locationManagerDidChangeAuthorization(_:))
+        } else {
+            sel = #selector(CLLocationManagerDelegate.locationManager(_:didChangeAuthorization:))
+        }
+        
+        return delegate.methodInvoked(sel)
+            .map { parameters in
+                return CLAuthorizationStatus(rawValue: parameters[1] as! Int32) ?? .notDetermined
+            }
+    }
+}
